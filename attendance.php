07a357b38d3b34dc6a2b6ebc656be8722468e7ac
gianestera <gian.estera@benilde.edu.ph>
2018-04-19 03:58:26 +0800

    attendance

--- attendance.php
+++ attendance.php
@@ -1,54 +1,44 @@
 <?php
-    $display = "none";
-    include('controllers/attendance_controller.php');
+  include('controllers/attendance_controller.php');
+
 ?>
 <!DOCTYPE html>
 <head>
 </head>
 <body>
-    <section id="main-content">
+ <section id="main-content">
         <section class="wrapper">
-            <div class="container">
-                <div class="row">
-                    <form class="form-horizontal" action="" method="post" name="upload_excel" enctype="multipart/form-data">
-                        <fieldset>
-                            <legend>Attendance CSV Upload</legend>
-                            <?php
-                                if(isset($_GET['upload']))
-                                {
-                                    echo "
-                                        <div class='col-lg-10 col-lg-offset-1'>
-                                            <div class='alert alert-danger alert-dismissable fade in'>
-                                                 <a href='' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
-                                                Please upload a valid CSV file.
-                                            </div>
-                                        </div>
-                                    ";
-                                }
-                            ?>
-                            <div class="form-group">
-                                <label class="col-md-4 control-label" for="filebutton">Select File</label>
-                                <div class="col-md-4">
-                                    <input type="file" name="file" id="file" class="input-large">
-                                </div>
+            <div class="row mt">
+                <div class="col-lg-12">
+                    <div class="form-panel">
+                    
+                    <fieldset>
+                        <!-- Form Name -->
+                        <legend>Upload Attendance</legend>
+                        <form class="form-horizontal" method="post" name="upload_csv" enctype="multipart/form-data">
+ 
+                        <!-- File Button -->
+                    
+                        <div class="form-group">
+                            <label class="col-md-4 control-label" for="filebutton">Select File</label>
+                            <div class="col-md-4">
+                                <input type="file" name="file" id="file" class="input-large">
                             </div>
-                            <div class="form-group">
-                                <label class="col-md-4 control-label" for="singlebutton"></label>
-                                <div class="col-md-4">
-                                    <button type="submit" id="submit" name="Import" class="btn btn-primary button-loading" data-loading-text="Loading...">Import</button>
-                                </div>
+                        </div>
+ 
+                        <!-- Button -->
+                        <div class="form-group">
+                            <label class="col-md-4 control-label" for="singlebutton">Import data</label>
+                            <div class="col-md-4">
+                                <button type="submit" id="submit" name="Import" class="btn btn-primary button-loading" data-loading-text="Loading...">Import</button>
                             </div>
-                        </fieldset>
-                    </form>
+                        </div>
+                     </form>
+                    </fieldset>
+                   </div>
                 </div>
             </div>
-            <div>
-                <?php
-                   get_all_records($con);
-                ?>
-            </div>
-        </div>
-    </div>
+        </section>
+    </section>
 </body>
- 
 </html>
--- controllers/attendance_controller.php
+++ controllers/attendance_controller.php
@@ -3,69 +3,44 @@
     include('function.php');
     include(loadHeader());
 
-   if(isset($_POST["Import"])){
 
-        echo $filename=$_FILES["file"]["tmp_name"];
-         if($_FILES["file"]["size"] > 0)
-         {
-            $file = fopen($filename, "r");
-             while (($getData = fgetcsv($file, 10000, ",")) !== FALSE)
+    if(isset($_POST["Import"])){
+
+
+            $csvMimes = array('text/x-comma-separated-values', 'text/comma-separated-values', 'application/octet-stream', 'application/vnd.ms-excel', 'application/x-csv', 'text/x-csv', 'text/csv', 'application/csv', 'application/excel', 'application/vnd.msexcel', 'text/plain' );
+            if(!empty($_FILES['file']['name']) && in_array($_FILES['file']['type'],$csvMimes))
+            {
+            
+            $filename=$_FILES["file"]["tmp_name"];      
+             if($_FILES["file"]["size"] > 0)
              {
-
-               $sql_insertAttendance = "INSERT INTO attendances ('accountID','attendanceIn','attendanceOut','attendanceDate') 
-                    values('$getData[0]','$getData[1]','$getData[2]','$getData[3]')";
-                var_dump($sql_insertAttendance);
-                    exit();
-            
-               $result = sqlsrv_query($con, $sql_insertAttendance);
-                if(!$result )
-                {
-                    echo "<script type=\"text/javascript\">
-                            alert(\"Invalid File:Please Upload CSV File.\");
+                $file = fopen($filename, "r");
+                while (($getData = fgetcsv($file, 10000, ",")) !== FALSE)
+                 {
+                   $sql_getAttendance = "INSERT into attendance (accountID,attendanceIn,attendanceOut,attendanceDate) 
+                       values ('".$getData[0]."','".$getData[1]."','".$getData[2]."','".$getData[3]."')";
+                       $stmt_getAttendance = sqlsrv_query($con, $sql_getAttendance);
+                    if(!isset($stmt_getAttendance))
+                    {
+                        echo "<script type=\"text/javascript\">
+                                alert(\"Invalid File:Please Upload CSV File.\");
+                                window.location = \"attendance.php\"
+                              </script>";       
+                    }
+                    else {
+                          echo "<script type=\"text/javascript\">
+                            alert(\"CSV File has been successfully Imported.\");
                             window.location = \"attendance.php\"
-                        </script>";     
-                }
-
+                        </script>";
+                    }
+                 }
+                
+                 fclose($file); 
              }
-             fclose($file);
-           
-             echo "<script type=\"text/javascript\">
-                        alert(\"CSV File has been successfully Imported.\");
-                        window.location = \"index.php\"
-                    </script>"; 
-            
-         }
+        }
     }
 
-    function get_all_records($con){
-   
 
-    $sql_getAttendance = "SELECT accountID, attendanceIn, attendanceOut, attendanceDate FROM attendances";
-    $stmt_getAttendance = sqlsrv_query($con, $sql_getAttendance);
-    if (sqlsrv_num_rows($stmt_getAttendance) > 0) {
-     echo "<div class='table-responsive'><table id='myTable' class='table table-striped table-bordered'>
-     <thead>
-     <tr>
-                        <th>Account ID</th>
-                        <th>Time In</th>
-                        <th>Time Out</th>
-                        <th>Date</th>
-                        </tr></thead><tbody>";
-
-     while($row = sqlsrv_fetch_array($stmt_getAttendance)) {
+   ?>
 
 
-         echo "<tr><td>" . $row['accountID']."</td>
-                   <td>" . $row['attendanceIn']."</td>
-                   <td>" . $row['attendanceOut']."</td>
-                   <td>" . $row['attendanceDate']."</td></tr>";
-         
-     }
-  
-     echo "</tbody></table></div>";
-     
-    } else {
-         echo "you have no records";
-    }
-}
-?>
